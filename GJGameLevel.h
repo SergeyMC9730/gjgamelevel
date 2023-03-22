26ba40a04a17161025289cce5a3e4992d30f0402
dogotrigger <bdsservers@yandex.ru>
2023-03-22 11:02:23 +0800

    Sync with levelapi

--- GJGameLevel.h
+++ GJGameLevel.h
@@ -3,32 +3,7 @@
 #include <string>
 #include <vector>
 
-#define PKSTRING 0
-#define PKINT 1
-#define PKBOOL 2
-#define PKBASE64 3
-
-#define PARSE_KEY_PKSTRING(keyN, member) \
-    case keyN: {\
-        member = std::string(parsed); \
-        break; \
-    }
-#define PARSE_KEY_PKINT(keyN, member) \
-    case keyN: {\
-        member = std::atoi(parsed); \
-        break; \
-    }
-#define PARSE_KEY_PKBOOL(keyN, member) \
-    case keyN: {\
-        member = (bool)(std::atoi(parsed)); \
-        break; \
-    }
-#define PARSE_KEY_PKBASE64(keyN, member) \
-    case keyN: {\
-        member = std::string((char *)base64_decode(std::string(parsed)).data()); \
-        break; \
-    }
-#define PARSE_KEY(keyN, member, vType) PARSE_KEY_##vType(keyN, member)
+#include "GJLevelObject.h"
 
 class GJGameLevel {
 public:
@@ -64,16 +39,15 @@ public:
     bool m_bGauntlet;
     bool m_b2P;
 
-    std::string m_sLevelName;
-    std::string m_sDescription;
-    std::string m_sLevelString;
-    std::string m_sXORPassword;
-    std::string m_sUploadDate;
-    std::string m_sUpdateDate;
-    std::string m_sExtraString;
-    std::string m_sSettings;
-    std::string m_sRecordString;
-    std::string m_sSondURL;
+    std::string *m_sLevelName;
+    std::string *m_sDescription;
+    std::string *m_sLevelString;
+    std::string *m_sXORPassword;
+    std::string *m_sUploadDate;
+    std::string *m_sUpdateDate;
+    std::string *m_sExtraString;
+    std::string *m_sSettings;
+    std::string *m_sRecordString;
 
     // Should be RobTop string
     static GJGameLevel *initWithFullLevel(std::string backendResponse, int compatibilityGV);
